c7c6ba1f9b0118df0204911553c25455ae00118c
Walter Tan <wtan@coursehero.com>
2017-03-23 16:58:12 -0700

    Check if any images are in view on initial load

--- js/progressive.js
+++ js/progressive.js
@@ -24,43 +24,46 @@
     var handleScroll = root.Lazy.createHandleScroll(imagesToLazyLoad, function (image) {
         // root.Lazy.loadImage(image)
         //     .then(function (imageThumb) {
-                var imageThumb = image
-                var canvas = imageThumb.nextElementSibling
-                var fragment
-                var imageFull
+        var imageThumb = image
+        var canvas = imageThumb.nextElementSibling
+        var fragment
+        var imageFull
 
-                root.StackBlur.blur(imageThumb, canvas, 3)
+        root.StackBlur.blur(imageThumb, canvas, 3)
 
-                // Create new image element to insert
-                imageFull = document.createElement('img')
-                imageFull.src = imageThumb.getAttribute('data-full')
-                imageFull.className = [
-                        PROGRESSIVE_CONSTS.progressiveFull, 
-                        PROGRESSIVE_CONSTS.opacityZero
-                    ].join(' ')
-                fragment = document.createDocumentFragment()
-                fragment.appendChild(imageFull)
+        // Create new image element to insert
+        imageFull = document.createElement('img')
+        imageFull.src = imageThumb.getAttribute('data-full')
+        imageFull.className = [
+                PROGRESSIVE_CONSTS.progressiveFull, 
+                PROGRESSIVE_CONSTS.opacityZero
+            ].join(' ')
+        fragment = document.createDocumentFragment()
+        fragment.appendChild(imageFull)
 
-                // Hide canvas and show full size image when fully loaded
-                imageFull.addEventListener(PROGRESSIVE_CONSTS.load, function loadFullSuccessListener() {
-                    imageFull.removeEventListener(PROGRESSIVE_CONSTS.load, loadFullSuccessListener)
+        // Hide canvas and show full size image when fully loaded
+        imageFull.addEventListener(PROGRESSIVE_CONSTS.load, function loadFullSuccessListener() {
+            imageFull.removeEventListener(PROGRESSIVE_CONSTS.load, loadFullSuccessListener)
 
-                    imageFull.className = [
-                        PROGRESSIVE_CONSTS.progressiveFull, 
-                        PROGRESSIVE_CONSTS.opacityOne
-                    ].join(' ')
-                    canvas.className = [
-                        PROGRESSIVE_CONSTS.progressiveCanvas, 
-                        PROGRESSIVE_CONSTS.opacityZero
-                    ].join(' ')
-                })
+            imageFull.className = [
+                PROGRESSIVE_CONSTS.progressiveFull, 
+                PROGRESSIVE_CONSTS.opacityOne
+            ].join(' ')
+            canvas.className = [
+                PROGRESSIVE_CONSTS.progressiveCanvas, 
+                PROGRESSIVE_CONSTS.opacityZero
+            ].join(' ')
+        })
 
-                canvas.parentElement.appendChild(fragment)
+        canvas.parentElement.appendChild(fragment)
             // })
             // .catch(function (data) {
             //     console.warn(data)
             // })
     })
+    
+    // Check if any images are already in view
+    imagesToLazyLoad.forEach(handleScroll)
 
     document.addEventListener(PROGRESSIVE_CONSTS.scroll, handleScroll)
 })(this)
